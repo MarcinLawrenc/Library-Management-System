7f50b77d3e2a85358991d74a292919bebe6d55f7
MarcinLawrenc <marcin.m.lawrenc@gmail.com>
2023-07-01 22:47:41 +0200

    modified:   CustomerBookBrowsing.css 	modified:   CustomerBookBrowsing.html 	modified:   CustomerBookBrowsing.js

--- CustomerBookBrowsing.js
+++ CustomerBookBrowsing.js
@@ -1,99 +1,56 @@
-// Fetch JSON data from the file
+// Fetch data from books.json
 fetch('books.json')
   .then(response => response.json())
   .then(data => {
-    // Access and use the data here
-    displayBooks(data);
+    const bookList = document.getElementById('book-list');
+
+    // Iterate over the books data
+    data.forEach(book => {
+      const bookItem = document.createElement('div');
+      bookItem.classList.add('book-item');
+
+      const bookThumbnail = document.createElement('img');
+      bookThumbnail.classList.add('book-thumbnail');
+      bookThumbnail.src = book.bookPhotoUrl || 'bookPhotos/bookphoto.jpg';
+
+      const bookTitle = document.createElement('h2');
+      bookTitle.textContent = book.bookTitle;
+
+      const bookAuthor = document.createElement('p');
+      bookAuthor.textContent = `${book.authorName} ${book.authorSurname}`;
+
+      bookItem.addEventListener('click', () => {
+        showBookDetails(book);
+      });
+
+      bookItem.appendChild(bookThumbnail);
+      bookItem.appendChild(bookTitle);
+      bookItem.appendChild(bookAuthor);
+
+      bookList.appendChild(bookItem);
+    });
   })
   .catch(error => {
-    console.log('Error:', error);
+    console.error('Error fetching book data:', error);
   });
 
-// Display books on the page
-function displayBooks(books) {
-  const bookList = document.getElementById('book-list');
+const bookDetailsContainer = document.getElementById('book-details-container');
+const bookDetails = document.getElementById('book-details');
+const closeButton = document.getElementById('close-button');
 
-  books.forEach(book => {
-    const bookItem = createBookItem(book);
-    bookList.appendChild(bookItem);
-  });
+closeButton.addEventListener('click', hideBookDetails);
+
+function showBookDetails(book) {
+  bookDetails.innerHTML = `
+    <h2>${book.bookTitle}</h2>
+    <p>${book.authorName} ${book.authorSurname}</p>
+    <p>${book.bookDescriptionLong}</p>
+  `;
+
+  bookDetailsContainer.style.display = 'block';
 }
 
-// Create book item
-function createBookItem(book) {
-  const bookItem = document.createElement('div');
-  bookItem.classList.add('book-item');
-  bookItem.style.backgroundImage = `url(${book.bookPhotoUrl})`;
-  bookItem.addEventListener('click', () => {
-    displayBookDetails(book);
-  });
-
-  const title = document.createElement('h2');
-  title.classList.add('book-title');
-  title.textContent = book.bookTitle;
-
-  const author = document.createElement('p');
-  author.classList.add('book-author');
-  author.textContent = `By ${book.authorName} ${book.authorSurname}`;
-
-  bookItem.appendChild(title);
-  bookItem.appendChild(author);
-
-  return bookItem;
-}
-
-// Display book details
-function displayBookDetails(book) {
-  const bookDetails = document.getElementById('book-details');
-  bookDetails.innerHTML = ''; // Clear previous details
-
-  const closeButton = document.createElement('button');
-  closeButton.classList.add('close-button');
-  closeButton.textContent = 'X';
-  closeButton.addEventListener('click', () => {
-    hideBookDetails(bookDetails);
-  });
-
-  const image = document.createElement('img');
-  image.classList.add('book-photo-large');
-  image.src = book.bookPhotoUrl;
-  image.alt = book.bookTitle;
-
-  const title = document.createElement('h2');
-  title.classList.add('book-title');
-  title.textContent = book.bookTitle;
-
-  const author = document.createElement('p');
-  author.classList.add('book-author');
-  author.textContent = `By ${book.authorName} ${book.authorSurname}`;
-
-  const description = document.createElement('p');
-  description.classList.add('book-description');
-  description.textContent = book.bookDescriptionLong;
-
-  bookDetails.appendChild(closeButton);
-  bookDetails.appendChild(image);
-  bookDetails.appendChild(title);
-  bookDetails.appendChild(author);
-  bookDetails.appendChild(description);
-
-  // Hide other books
-  const bookItems = document.getElementsByClassName('book-item');
-  Array.from(bookItems).forEach(item => {
-    item.style.display = 'none';
-  });
-
-  // Show book details
-  bookDetails.style.display = 'block';
-}
-
-// Hide book details and show all books
 function hideBookDetails() {
-  const bookDetails = document.getElementById('book-details');
-  bookDetails.style.display = 'none';
-
-  const bookItems = document.getElementsByClassName('book-item');
-  Array.from(bookItems).forEach(item => {
-    item.style.display = 'inline-block';
-  });
+  bookDetails.innerHTML = '';
+  bookDetailsContainer.style.display = 'none';
 }

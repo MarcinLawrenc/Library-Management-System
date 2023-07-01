88155bea7eeb40509f72b8dda61b722c9a4e16eb
MarcinLawrenc <marcin.m.lawrenc@gmail.com>
2023-07-01 16:57:23 +0200

    fixed login

--- script.js
+++ script.js
@@ -10,15 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
   // Login form submission
   document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("loginPassword").value;
 
-    if (username === "admin" && password === "password") {
+    const users = getUsers();
+    const user = users.find(function(user) {
+      return user.email === email && user.password === password && user.userType === "admin";
+    });
+
+    if (user) {
       localStorage.setItem("loggedIn", "true");
       showUserManagement();
       displayUsers();
     } else {
-      displayMessage("error", "Invalid username or password");
+      displayMessage("error", "Invalid email or password");
     }
   });
 
@@ -26,12 +31,12 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("userForm").addEventListener("submit", function(e) {
     e.preventDefault();
     const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const password = document.getElementById("userPassword").value;
     const name = document.getElementById("name").value;
     const surname = document.getElementById("surname").value;
     const userType = document.getElementById("userType").value;
 
-    const newUser = {
+    const user = {
       email: email,
       password: password,
       name: name,
@@ -39,41 +44,20 @@ document.addEventListener("DOMContentLoaded", function() {
       userType: userType
     };
 
-    addUser(newUser);
+    addUser(user);
     displayUsers();
     resetForm();
     displayMessage("success", "User added successfully!");
   });
 
-  // Delete user button click
-  document.getElementById("userTable").addEventListener("click", function(e) {
-    if (e.target.classList.contains("delete-btn")) {
-      const email = e.target.getAttribute("data-email");
-      deleteUser(email);
-      displayUsers();
-      displayMessage("success", "User deleted successfully!");
-    }
-  });
-
-  // Edit user button click
-  document.getElementById("userTable").addEventListener("click", function(e) {
-    if (e.target.classList.contains("edit-btn")) {
-      const email = e.target.getAttribute("data-email");
-      const user = getUser(email);
-      if (user) {
-        showEditForm(user);
-      }
-    }
-  });
-
-  // Update user form submission
-  document.getElementById("userForm").addEventListener("submit", function(e) {
+  // Edit form submission
+  document.getElementById("editForm").addEventListener("submit", function(e) {
     e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const name = document.getElementById("name").value;
-    const surname = document.getElementById("surname").value;
-    const userType = document.getElementById("userType").value;
+    const email = document.getElementById("editEmail").value;
+    const password = document.getElementById("editPassword").value;
+    const name = document.getElementById("editName").value;
+    const surname = document.getElementById("editSurname").value;
+    const userType = document.getElementById("editUserType").value;
 
     const updatedUser = {
       email: email,
@@ -83,31 +67,66 @@ document.addEventListener("DOMContentLoaded", function() {
       userType: userType
     };
 
-    updateUser(email, updatedUser);
+    updateUser(updatedUser);
     displayUsers();
-    resetForm();
     displayMessage("success", "User updated successfully!");
+    hideEditForm();
   });
 
-  // Cancel edit button click
-  document.getElementById("userForm").addEventListener("click", function(e) {
-    if (e.target.classList.contains("cancel-btn")) {
-      resetForm();
-      displayMessage("", "");
+  // Edit and Delete button clicks
+  document.querySelector("#userTable tbody").addEventListener("click", function(e) {
+    if (e.target.classList.contains("edit-btn")) {
+      const email = e.target.getAttribute("data-email");
+      const user = getUser(email);
+      if (user) {
+        showEditForm(user);
+      }
+    } else if (e.target.classList.contains("delete-btn")) {
+      const email = e.target.getAttribute("data-email");
+      deleteUser(email);
+      displayUsers();
+      displayMessage("success", "User deleted successfully!");
     }
   });
 
-  // Functions
+  // Cancel button click
+  document.getElementById("userForm").addEventListener("click", function(e) {
+    if (e.target.classList.contains("cancel-btn")) {
+      resetForm();
+    }
+  });
+
+  // Show login form
   function showLoginForm() {
     document.getElementById("loginForm").style.display = "block";
     document.getElementById("userForm").style.display = "none";
     document.getElementById("userTable").style.display = "none";
+    document.getElementById("editFormContainer").style.display = "none";
+    document.getElementById("message").style.display = "none";
   }
 
+  // Show user management form
   function showUserManagement() {
     document.getElementById("loginForm").style.display = "none";
     document.getElementById("userForm").style.display = "block";
     document.getElementById("userTable").style.display = "block";
+    document.getElementById("editFormContainer").style.display = "none";
+    document.getElementById("message").style.display = "block";
+  }
+
+  // Show edit form
+  function showEditForm(user) {
+    document.getElementById("editEmail").value = user.email;
+    document.getElementById("editPassword").value = user.password;
+    document.getElementById("editName").value = user.name;
+    document.getElementById("editSurname").value = user.surname;
+    document.getElementById("editUserType").value = user.userType;
+    document.getElementById("editFormContainer").style.display = "block";
+  }
+
+  // Hide edit form
+  function hideEditForm() {
+    document.getElementById("editFormContainer").style.display = "none";
   }
 
   function displayUsers() {
@@ -115,7 +134,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const tableBody = document.querySelector("#userTable tbody");
     tableBody.innerHTML = "";
 
-    if (users) {
+    if (users && users.length > 0) {
       users.forEach(function(user) {
         const row = document.createElement("tr");
         row.innerHTML = `
@@ -134,46 +153,10 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-  function displayMessage(className, message) {
-    const messageDiv = document.createElement("div");
-    messageDiv.className = className;
+  function displayMessage(type, message) {
+    const messageDiv = document.getElementById("message");
     messageDiv.textContent = message;
-    document.body.insertBefore(messageDiv, document.getElementById("userForm"));
-    setTimeout(function() {
-      messageDiv.remove();
-    }, 3000);
-  }
-
-  function addUser(user) {
-    const users = getUsers() || [];
-    users.push(user);
-    localStorage.setItem("users", JSON.stringify(users));
-  }
-
-  function deleteUser(email) {
-    const users = getUsers() || [];
-    const updatedUsers = users.filter(function(user) {
-      return user.email !== email;
-    });
-    localStorage.setItem("users", JSON.stringify(updatedUsers));
-  }
-
-  function getUser(email) {
-    const users = getUsers() || [];
-    return users.find(function(user) {
-      return user.email === email;
-    });
-  }
-
-  function updateUser(email, updatedUser) {
-    const users = getUsers() || [];
-    const updatedUsers = users.map(function(user) {
-      if (user.email === email) {
-        return updatedUser;
-      }
-      return user;
-    });
-    localStorage.setItem("users", JSON.stringify(updatedUsers));
+    messageDiv.className = type;
   }
 
   function getUsers() {
@@ -195,41 +178,55 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-  function resetForm() {
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
-    document.getElementById("name").value = "";
-    document.getElementById("surname").value = "";
-    document.getElementById("userType").value = "admin";
-    document.getElementById("addUser").style.display = "block";
-    document.getElementById("addUser").textContent = "Add User";
-    document.getElementById("userForm").removeEventListener("submit", updateUser);
-    document.getElementById("userForm").addEventListener("submit", addUser);
+  function addUser(user) {
+    let users = getUsers();
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
   }
 
-  function showEditForm(user) {
-    document.getElementById("email").value = user.email;
-    document.getElementById("password").value = user.password;
-    document.getElementById("name").value = user.name;
-    document.getElementById("surname").value = user.surname;
-    document.getElementById("userType").value = user.userType;
-    document.getElementById("addUser").style.display = "none";
-    document.getElementById("addUser").textContent = "Update User";
-    document.getElementById("userForm").removeEventListener("submit", addUser);
-    document.getElementById("userForm").addEventListener("submit", function(e) {
-      e.preventDefault();
-      const email = document.getElementById("email").value;
-      const updatedUser = {
-        email: email,
-        password: document.getElementById("password").value,
-        name: document.getElementById("name").value,
-        surname: document.getElementById("surname").value,
-        userType: document.getElementById("userType").value
-      };
-      updateUser(email, updatedUser);
-      displayUsers();
-      resetForm();
-      displayMessage("success", "User updated successfully!");
+  function getUser(email) {
+    const users = getUsers();
+    return users.find(function(user) {
+      return user.email === email;
     });
   }
+
+  function updateUser(updatedUser) {
+    let users = getUsers();
+    users = users.map(function(user) {
+      if (user.email === updatedUser.email) {
+        return updatedUser;
+      }
+      return user;
+    });
+    localStorage.setItem("users", JSON.stringify(users));
+  }
+
+  function deleteUser(email) {
+    let users = getUsers();
+    users = users.filter(function(user) {
+      return user.email !== email;
+    });
+    localStorage.setItem("users", JSON.stringify(users));
+  }
+
+  function resetForm() {
+    document.getElementById("email").value = "";
+    document.getElementById("userPassword").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("userType").selectedIndex = 0;
+  }
+
+  // Fetch user data from users.json file
+  fetch("users.json")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      localStorage.setItem("users", JSON.stringify(data));
+    })
+    .catch(function(error) {
+      console.log("Error fetching user data:", error);
+    });
 });
